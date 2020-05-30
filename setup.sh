b8a70ddb905d25009d5b30e7d8a32fb68f75789e
virajshastri97 <viraj.shastri@colostate.edu>
2020-05-30 04:16:14 -0600

    Rewrite setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
-#!/bin/bash
-for f in ./.*; do
-  if [ -f $f ] && [ $f != ".DS_Store" ]; then
-    echo Copying $(basename "$f" ".");
-    [ -f "~/${f}" ] && mv "~/$f" "~/$f.bkp";
-    cp "./$f" ~/;
-  fi
+#!/bin/sh
+
+# Copying common files
+copy () {
+  cp $PWD/$1 ~/$1 && echo Copying $1
+}
+
+unset flist
+flist=(".aliases" ".prompt" ".gitconfig" ".tmux.conf" ".vimrc" ".inputrc")
+
+for f in ${flist[@]}; do
+  [ -f ~/$f ] && mv ~/$f ~/$f.bkp
+  copy $f
 done
+
+# Copying shell specific files
+[[ $SHELL == *"zsh"* ]] && { copy ".zsh"; copy ".zprofile"; }
+[[ $SHELL == *"bash"* ]] && { copy ".bashrc"; copy ".bash_profile"; }
+
+# Copy alacritty config if using
+read -p "Are you using alacritty (y/n): " -n 1 -r
+echo
+[[ $REPLY =~ ^[Yy]$ ]] && copy "alacritty.yml"
