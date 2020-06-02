e30b4dbc9f6f84ae79e1cdecdef0991f4758f182
virajshastri97 <viraj.shastri@colostate.edu>
2020-06-01 20:56:08 -0600

    Change setup

--- setup.sh
+++ setup.sh
@@ -5,19 +5,8 @@ copy () {
   cp $PWD/$1 ~/$1 && echo Copying $1
 }
 
-unset flist
-flist=(".aliases" ".prompt" ".gitconfig" ".tmux.conf" ".vimrc" ".inputrc")
-
-for f in ${flist[@]}; do
-  [ -f ~/$f ] && mv ~/$f ~/$f.bkp
-  copy $f
-done
+cp -r .config $HOME/ && echo Copying config dir
 
 # Copying shell specific files
-[[ $SHELL == *"zsh"* ]] && { copy ".zsh"; copy ".zprofile"; }
-[[ $SHELL == *"bash"* ]] && { copy ".bashrc"; copy ".bash_profile"; }
-
-# Copy alacritty config if using
-read -p "Are you using alacritty (y/n): " -n 1 -r
-echo
-[[ $REPLY =~ ^[Yy]$ ]] && copy "alacritty.yml"
+[[ $SHELL == *"zsh"* ]] && copy ".zprofile"
+[[ $SHELL == *"bash"* ]] && copy ".bash_profile"
