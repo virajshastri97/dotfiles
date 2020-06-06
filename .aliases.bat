3bc6623d0879df70fa46426aff7ba7ef96494b8b
virajshastri97 <viraj.shastri@colostate.edu>
2020-06-06 00:53:50 -0600

    Add aliases file for windows cmd

---
+++ .aliases.bat
@@ -0,0 +1,11 @@
+@echo off
+doskey g=git
+doskey gs=git s
+doskey gp=git p
+doskey gpo=git po
+doskey gpu=git pu
+doskey gc=git c
+doskey gone=git one
+doskey ga=git a
+doskey gd=git d
+doskey activate=venv\Scripts\activate.bat
